cbf09f0d901ff41c06b91a349804e0ec99c656fc
AASHISH RANJAN <44123630+aashishr7@users.noreply.github.com>
2025-05-06 20:34:25 +0530

    Update uvm_sequencer.sv

--- uvm_sequencer.sv
+++ uvm_sequencer.sv
@@ -1,42 +1,20 @@
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 
-class ram_sequence extends uvm_sequence#(seq_item);
+class ram_sequencer extends uvm_sequencer#(seq_item);
   
-  `uvm_object_utils(ram_sequence)
+  `uvm_component_utils(ram_sequencer)
   
-  seq_item txn;
-  
-  function new(string name ="ram_sequence");
-    super.new(name);
+  function new(string name="ram_sequencer", uvm_component parent);
+    super.new(name, parent);
+    
   endfunction
   
-
-  
-  task body();
-  
-    repeat(1) begin
-      txn = seq_item::type_id::create("txn");
-      $display("Created object of type :: SEQ %s", txn.get_type_name());
-      
-      start_item(txn);
-      
-      if(!txn.randomize()) begin
-        `uvm_error(get_type_name(), "!! Randomization failed !!");
-      end
-      else begin
-       // $display( "SEQ::  paddr=%0d, pwdata=%0d, wr_en=%0d, psel=%0d, pen=%0d, prdata=%0d\n",txn.paddr, txn.pwdata, txn.wr_en, txn.psel, txn.pen, txn.prdata);
-        `uvm_info(get_type_name(), "!! Randomization success !!", UVM_HIGH);
-      end
-      
-      finish_item(txn);
-      #10;
-      
-    end
-    
-  endtask
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    `uvm_info(get_type_name(), "RAM:: SEQUENCER", UVM_HIGH);
+    $display("Inside build phase SEQR= %s", get_type_name());
+  endfunction
   
 endclass
-      
-      
-      
+    
